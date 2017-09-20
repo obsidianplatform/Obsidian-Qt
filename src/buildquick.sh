4c9693b6acb32e27d70037d31a1bd2fb75845076
Shad Lest <shad@obsidianplatform.com>
2017-09-20 14:13:41 -0700

    Updating build scripts for proper linux builds

--- src/buildquick.sh
+++ src/buildquick.sh
@@ -7,8 +7,11 @@ NPROC=$(nproc)
 echo "nproc: $NPROC"
 
 make -j$NPROC -f makefile.unix
-cp obsidiand /usr/bin/obsidiand
+sudo cp obsidiand /usr/bin/obsidiand
 
 echo "Build complete...copied src/obsidiand to /usr/bin/obsidiand"
+echo "Run the app by typing 'obsidiand' at the command prompt"
+echo "Follow the instructions to create an rpcuser and rpcpassword"
+echo "and run again to sync"
 exit 0
 
